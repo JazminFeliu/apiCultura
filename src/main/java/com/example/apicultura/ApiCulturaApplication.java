548dc43b9367aa7f69f8815157eaaa2d078eb26d
JazminFeliu <jazminfeliu@gmail.com>
2021-06-19 09:26:28 -0300

    Subo ej de APi Cultura Lista de Museos Argentina

---
+++ src/main/java/com/example/apicultura/ApiCulturaApplication.java
@@ -0,0 +1,13 @@
+package com.example.apicultura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCulturaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiCulturaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/apicultura/ApiCulturaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apicultura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCulturaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
